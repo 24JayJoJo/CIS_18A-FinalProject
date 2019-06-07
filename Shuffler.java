1c8c877d4a1995de8a992d749cba24c71695b679
lmares037 <50275444+lmares037@users.noreply.github.com>
2019-06-06 17:49:18 -0700

    Update Shuffler.java

--- Shuffler.java
+++ Shuffler.java
@@ -3,20 +3,52 @@ package poker;
 
 
 import java.util.ArrayList;
-import java.util.Random;
 
-public class Shuffler<T> {
 
-	
 
-	public ArrayList<T> shuffle(ArrayList<T> deck) {
-		ArrayList<T> shuffledDeck = new ArrayList<T>();
-		Random r = new Random(System.currentTimeMillis());
-		while (!deck.isEmpty()) {
-			int index = Math.abs(r.nextInt()%deck.size());
-			shuffledDeck.add(deck.remove(index));
+public class CardDeck 
+{
+	//Creates array to store deck cards
+	private ArrayList<Card> deck = new ArrayList<>();
+	//Creates all possible cards in deck 
+	private CardDeck(String[] suits, String[] ranks) 
+        {
+		
+		for (String suit : suits) 
+                {
+			for (String rank : ranks) {
+				deck.add(new Card(suit, rank));
+			}
 		}
-		return shuffledDeck;
 	}
-
+	//checks if deck is empty or not
+	public Card deal() 
+        {
+		if (!deck.isEmpty()) 
+                {
+			return deck.remove(0);
+		} else {
+			return null;
+		}
+	}
+	//Returns a standard deck when called
+	public static CardDeck getStandardDeck() 
+        {
+		String[] suits = {"Spades", "Clubs", "Hearts", "Diamonds"};
+		String[] ranks = {"2", "3", "4", "5", "6", "7", "8",
+							"9", "10", "J", "Q", "K", "A"};
+		return new CardDeck(suits, ranks);
+	}
+	
+		
+		
+	
+	//shuffles cards in deck 
+	public void shuffle() 
+        {
+		Shuffler<Card> s = new Shuffler<>();
+		deck = s.shuffle(deck);
+	}
+	
+	
 }
