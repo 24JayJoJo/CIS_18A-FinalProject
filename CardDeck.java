2c000b6a6f593c31ebcf4bc168cd551ec644db6d
lmares037 <50275444+lmares037@users.noreply.github.com>
2019-06-06 17:46:58 -0700

    Update CardDeck.java

--- CardDeck.java
+++ CardDeck.java
@@ -6,29 +6,34 @@ import java.util.ArrayList;
 
 
 
-public class CardDeck {
+public class CardDeck 
+{
 	//Creates array to store deck cards
 	private ArrayList<Card> deck = new ArrayList<>();
 	//Creates all possible cards in deck 
-	private CardDeck(String[] suits, String[] ranks) {
-		//for (int i = 0; i < suits.length; ++i) {
-		//String suit = suits[i];
-		for (String suit : suits) {
+	private CardDeck(String[] suits, String[] ranks) 
+        {
+		
+		for (String suit : suits) 
+                {
 			for (String rank : ranks) {
 				deck.add(new Card(suit, rank));
 			}
 		}
 	}
 	//checks if deck is empty or not
-	public Card deal() {
-		if (!deck.isEmpty()) {
+	public Card deal() 
+        {
+		if (!deck.isEmpty()) 
+                {
 			return deck.remove(0);
 		} else {
 			return null;
 		}
 	}
 	//Returns a standard deck when called
-	public static CardDeck getStandardDeck() {
+	public static CardDeck getStandardDeck() 
+        {
 		String[] suits = {"Spades", "Clubs", "Hearts", "Diamonds"};
 		String[] ranks = {"2", "3", "4", "5", "6", "7", "8",
 							"9", "10", "J", "Q", "K", "A"};
@@ -39,7 +44,8 @@ public class CardDeck {
 		
 	
 	//shuffles cards in deck 
-	public void shuffle() {
+	public void shuffle() 
+        {
 		Shuffler<Card> s = new Shuffler<>();
 		deck = s.shuffle(deck);
 	}
